a0801b46232cee6b3c173f5ab7549792a47e67a3
Angelo Lustosa de Figueiredo Filho <01337098337@DIGITALCOLLEGE.LOCAL>
2024-05-27 21:56:56 -0300

    Ajuste na routa

--- routes/produtos.routes.js
+++ routes/produtos.routes.js
@@ -4,9 +4,11 @@ import { produtoService } from '../services/produtos.service.js'
 export const produtoRoute = (app) => {
     var route = express.Router()
 
+    route.post('/', produtoService.create);
     route.get('/', produtoService.getAll);
     route.get('/:id', produtoService.getById);
-    route.post('/', produtoService.create);
+    route.put('/:id', produtoService.update);
+    route.delete('/:id', produtoService.delete);
 
     app.use('/api/produto', route)
 }
